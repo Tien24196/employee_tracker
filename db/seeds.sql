30810d4f2f27418f5618175365435add940b9c9e
Tien Ngo <ngoquoctien24196@gmail.com>
2022-08-22 00:33:06 -0400

    create database and tables

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO department (name)
+VALUES
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales');
+
+INSERT INTO role (title, department_id, salary)
+VALUES
+  ('Sales Lead', 4, 100000),
+  ('Salesperson', 4, 80000),
+  ('Lead Engineer', 1, 150000),
+  ('Software Engineer', 1, 120000),
+  ('Account Manager', 2, 160000),
+  ('Accountant', 2, 125000),
+  ('Legal Team Lead', 3, 250000),
+  ('Lawyer', 3, 190000);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, NULL),
+  ('Virginia', 'Woolf', 2, 1),
+  ('Piers', 'Gaveston', 3, NULL),
+  ('Charles', 'LeRoi', 4, 3),
+  ('Katherine', 'Mansfield', 5, NULL),
+  ('Dora', 'Carrington', 6, 5),
+  ('Edward', 'Bellamy', 7, NULL),
+  ('Montague', 'Summers', 8, 7);
+
+  
